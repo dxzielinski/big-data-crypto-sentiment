6dc506ec5528be1d77a2517258d2d85849e5750f
MikolajKita <mikolajkita@gmail.com>
2025-11-22 18:32:03 +0100

    Add pubsub terraform file, limited tweet capture to most important fields

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -38,3 +38,28 @@ variable "docker_repo_id" {
   type        = string
   default     = "big-data-crypto-sentiment-repo"
 }
+
+variable "pubsub_topics" {
+  description = "Map of all data pipelines to create"
+  type = map(object({
+    topic_name   = string
+    retention    = optional(string, "604800s") # Default 7 days
+    ack_deadline = optional(number, 20)
+  }))
+
+  default = {
+    # Pipeline 1: Twitter
+    twitter = {
+      topic_name = "crypto-tweets-stream"
+    }
+    # Pipeline 2: Market Prices
+    prices = {
+      topic_name = "crypto-prices-stream"
+    }
+    # Pipeline 3: Technical Analysis
+    ta = {
+      topic_name = "crypto-ta-indicators"
+      retention  = "86400s"
+    }
+  }
+}
